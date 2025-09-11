5768864024170626bb8269b172a3437d91ac3d5a
Luis Riviere <larivier@syr.edu>
2025-09-11 10:21:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Multi-Page Labs", page_icon="🧪")
 
 lab2 = st.Page("lab2.py", title="Lab 2", icon="🧪")
 lab1 = st.Page("lab1.py", title="Lab 1", icon="🧪")
-lab3 = st.Page("lab3.py", title="Lab 3", icon="🧪")
+lab3 = st.Page("lab3.py", title="Lab 3", icon="💬")
 
 nav = st.navigation(pages=[lab2, lab1])   
 nav.run()
