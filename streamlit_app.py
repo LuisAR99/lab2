68adeaea6c1b39aa4d79509147986271a7bdd911
Luis Riviere <larivier@syr.edu>
2025-09-25 10:47:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ lab2 = st.Page("lab2.py", title="Lab 2", icon="🧪")
 lab1 = st.Page("lab1.py", title="Lab 1", icon="📄")
 lab3 = st.Page("lab3.py", title="Lab 3", icon="💬")
 lab4 = st.Page("lab4.py", title="Lab 4", icon="💬")
+lab5 = st.Page("lab5.py", title="Lab 5", icon="💬")
 
-nav = st.navigation(pages=[lab2, lab1, lab3, lab4])   
+nav = st.navigation(pages=[lab2, lab1, lab3, lab4, lab5])   
 nav.run()
